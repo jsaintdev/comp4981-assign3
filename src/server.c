913173cb7dd0ced1094e7fd1560750bfd650580b
justinsaint <jsaintdev@gmail.com>
2025-04-19 13:17:19 -0700

    Added meow to built-in server logic

--- src/server.c
+++ src/server.c
@@ -463,6 +463,10 @@ static p101_fsm_state_t execute_built_in(const struct p101_env *env, struct p101
     {
         process_type(client);
     }
+    else if(strcmp(client->cmd, "meow") == 0)
+    {
+        process_meow(client);
+    }
     else if(strcmp(client->cmd, "exit") == 0)
     {
         process_exit();
